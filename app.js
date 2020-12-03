0238d223e8c7b42d9fec1ff11286f9f9daea4500
Sam Marxz <sammarxzz@gmail.com>
2020-12-03 15:00:38 -0300

    hotfix variable name.

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 var express = require('express');
-var path = require('path');
 var logger = require('morgan');
 var cors = require('cors');
 
 require('./config/database');
 
-var corOptions = {
+var corsOptions = {
   origin: 'http://localhost:8000',
   optionsSuccessStatus: 200
 };
@@ -18,7 +17,6 @@ var app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors(corsOptions));
 
 app.use('/users', usersRouter);
