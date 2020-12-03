f8f74017e7bd2029e70e3bbe2e232aa28a6009de
Sam Marxz <sammarxzz@gmail.com>
2020-12-03 14:45:54 -0300

    Add cors.

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
+var cors = require('cors');
 
 require('./config/database');
+
+var corOptions = {
+  origin: 'http://localhost:8000',
+  optionsSuccessStatus: 200
+};
 
 var usersRouter = require('./app/routes/users');
 var notesRouter = require('./app/routes/notes');
@@ -13,6 +19,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors(corsOptions));
 
 app.use('/users', usersRouter);
 app.use('/notes', notesRouter);
