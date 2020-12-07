c14e5605bba513cd3fc1a0fcb2b1a5e984466f46
Sam Marxz <sammarxzz@gmail.com>
2020-12-07 20:31:54 -0300

    add Color Backup.

--- frontend/src/styles/Helpers/Colors/index.js
+++ frontend/src/styles/Helpers/Colors/index.js
@@ -1,7 +1,35 @@
 import { css } from 'styled-components';
 import { getAllCSSColors } from '../../../utils';
 
-const varColors = getAllCSSColors();
+const colorsBackup = [
+  "--color-blue-01",
+  "--color-blue-02",
+  "--color-blue-03",
+  "--color-blue-04",
+  "--color-blue-06",
+  "--color-black-08",
+  "--color-black-07",
+  "--color-black-06",
+  "--color-black-05",
+  "--color-black-04",
+  "--color-black-03",
+  "--color-black-02",
+  "--color-black-01",
+  "--color-grey-01",
+  "--color-grey-02",
+  "--color-grey-03",
+  "--color-grey-04",
+  "--color-grey-05",
+  "--color-grey-06",
+  "--color-grey-07",
+  "--color-grey-08",
+  "--color-red",
+  "--color-green",
+  "--color-base-white",
+  "--color-base-black"
+];
+
+let varColors = getAllCSSColors().lnegth > 0 ? getAllCSSColors() : colorsBackup;
 
 const Colors = css`
   ${varColors.map((color) => css`
