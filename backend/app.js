863f27e6e0ec2bf9d99167c09588af8a774eedca
Sam Marxz <sammarxzz@gmail.com>
2020-12-09 15:13:30 -0300

    Add global cors.

--- backend/app.js
+++ backend/app.js
@@ -4,11 +4,6 @@ var cors = require('cors');
 
 require('./config/database');
 
-var corsOptions = {
-  origin: ['http://localhost:3000', 'https://notly-client.herokuapp.com/'],
-  optionsSuccessStatus: 200
-};
-
 var usersRouter = require('./app/routes/users');
 var notesRouter = require('./app/routes/notes');
 
@@ -17,7 +12,7 @@ var app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use('/users', usersRouter);
 app.use('/notes', notesRouter);
